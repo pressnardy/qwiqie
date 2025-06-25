a618d53b62b02c798775d19f29273888cc31a54c
pressnardy <zimmarish@gmail.com>
2025-06-25 08:01:33 +0300

    stopped staticfiles

--- qwiqie/settings.py
+++ qwiqie/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
