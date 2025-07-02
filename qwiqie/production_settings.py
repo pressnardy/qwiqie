4059edd7b89fd911874e5c0376a6acbc6fc69b64
pressnardy <zimmarish@gmail.com>
2025-07-02 08:40:42 +0300

    changed db to postgreSQL

--- qwiqie/production_settings.py
+++ qwiqie/production_settings.py
@@ -25,9 +25,8 @@ SECRET_KEY = 'django-insecure-f$y@8$$e77$-guy*c!5u*(4tdwuas5mryqru1#e5qfvpq)id#+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['cosmic-midge-presumably.ngrok-free.app', '127.0.0.1',]
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [str(host).strip() for host in os.environ.get('ALLOWED_HOSTS').split(',')]
 # Application definition
 
 INSTALLED_APPS = [
@@ -79,27 +78,27 @@ WSGI_APPLICATION = 'qwiqie.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('DB_USER'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST'),
-#         'PORT': os.environ.get('DB_PORT', '25060'),
-#         'OPTIONS': {
-#             'sslmode': 'require',
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '25060'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
