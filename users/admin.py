a56d381d1e25c4baacdf9926a3990b8f5d1a5e70
pressnardy <zimmarish@gmail.com>
2025-07-25 21:51:29 +0300

    remove T&C models

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-from users.models import TermsAndConditions
+
 # Register your models here.
 
 admin.site.site_title = "Admin"
 admin.site.site_header = "Qwiqee.com Admin"
 admin.site.index_title = "Welcome to My Admin Portal"
-
-@admin.register(TermsAndConditions)
-class TermsAndConditionsAdmin(admin.ModelAdmin):
-    list_display =['name', 'pdf_file']
 
